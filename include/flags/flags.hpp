a6153c75d3c0b8eb7c5455e7e3b468027ee17bc9
Curve <fynnbwdt@gmail.com>
2023-06-13 23:37:58 +0200

    feat: initial commit

---
+++ include/flags/flags.hpp
@@ -0,0 +1,124 @@
+#pragma once
+#include <concepts>
+#include <type_traits>
+
+namespace flags
+{
+    template <class T>
+    concept scoped_enum = requires() {
+        requires std::is_enum_v<T>;
+        requires not std::is_convertible_v<T, std::underlying_type_t<T>>;
+    };
+
+    template <class T>
+        requires scoped_enum<T>
+    static constexpr bool enabled = false;
+
+    template <class T>
+        requires enabled<T>
+    static constexpr bool no_underlying = false;
+
+    template <class T>
+    concept allowed = requires() {
+        requires enabled<T>;
+        requires scoped_enum<T>;
+        requires std::integral<std::underlying_type_t<T>>;
+    };
+
+    template <typename T>
+    constexpr auto underlying(T value)
+    {
+        if constexpr (scoped_enum<T>)
+        {
+            return static_cast<std::underlying_type_t<T>>(value);
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    template <typename Enum, typename T>
+    constexpr auto cast(T value)
+    {
+        if constexpr (no_underlying<Enum>)
+        {
+            return static_cast<Enum>(value);
+        }
+        else
+        {
+            return value;
+        }
+    }
+} // namespace flags
+
+template <typename T>
+    requires flags::allowed<T>
+constexpr auto operator~(T value)
+{
+    return flags::cast<T>(~flags::underlying(value));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto operator&(L left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return flags::cast<enum_t>(flags::underlying(left) & flags::underlying(right));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto operator|(L left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return flags::cast<enum_t>(flags::underlying(left) | flags::underlying(right));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto operator^(L left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return flags::cast<enum_t>(flags::underlying(left) ^ flags::underlying(right));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto operator<<(L left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return flags::cast<enum_t>(flags::underlying(left) << flags::underlying(right));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto operator>>(L left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return flags::cast<enum_t>(flags::underlying(left) >> flags::underlying(right));
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto &operator&=(L &left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return left = flags::cast<enum_t>(left & right);
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto &operator|=(L &left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return left = flags::cast<enum_t>(left | right);
+}
+
+template <typename L, typename R>
+    requires flags::allowed<L> or flags::allowed<R>
+constexpr auto &operator^=(L &left, R right)
+{
+    using enum_t = std::conditional_t<flags::allowed<L>, L, R>;
+    return left = flags::cast<enum_t>(left ^ right);
+}
---
+++ tests/basic.test.cpp
@@ -0,0 +1,80 @@
+#include <catch2/catch_test_macros.hpp>
+#include <flags/flags.hpp>
+
+enum class my_enum
+{
+    a = 1 << 0,
+    b = 1 << 1,
+    c = 1 << 2,
+};
+
+template <>
+static constexpr bool flags::enabled<my_enum> = true;
+
+TEST_CASE("Basic bit-wise operations are tested", "[Basic]")
+{
+    auto flag = my_enum::a | my_enum::b;
+    flag |= my_enum::c;
+
+    REQUIRE(flag & my_enum::a);
+    REQUIRE(flag & my_enum::b);
+    REQUIRE(flag & my_enum::c);
+
+    flag &= ~my_enum::c;
+
+    REQUIRE(flag & my_enum::a);
+    REQUIRE(flag & my_enum::b);
+    REQUIRE(not(flag & my_enum::c));
+
+    flag &= ~my_enum::b;
+
+    REQUIRE(flag & my_enum::a);
+    REQUIRE(not(flag & my_enum::b));
+    REQUIRE(not(flag & my_enum::c));
+
+    flag &= ~my_enum::a;
+
+    REQUIRE(not(flag & my_enum::a));
+    REQUIRE(not(flag & my_enum::b));
+    REQUIRE(not(flag & my_enum::c));
+}
+
+enum class my_enum_2
+{
+    a = 1 << 0,
+    b = 1 << 1,
+    c = 1 << 2,
+};
+
+template <>
+static constexpr bool flags::enabled<my_enum_2> = true;
+template <>
+static constexpr bool flags::no_underlying<my_enum_2> = true;
+
+TEST_CASE("Basic bit-wise operations are tested [no_underlying]", "[Basic-No-Underlying]")
+{
+    auto flag = my_enum_2::a | my_enum_2::b;
+    flag |= my_enum::c;
+
+    REQUIRE((flag & my_enum_2::a) == my_enum_2::a);
+    REQUIRE((flag & my_enum_2::b) == my_enum_2::b);
+    REQUIRE((flag & my_enum_2::c) == my_enum_2::c);
+
+    flag &= ~my_enum_2::c;
+
+    REQUIRE((flag & my_enum_2::a) == my_enum_2::a);
+    REQUIRE((flag & my_enum_2::b) == my_enum_2::b);
+    REQUIRE((flag & my_enum_2::c) != my_enum_2::c);
+
+    flag &= ~my_enum_2::b;
+
+    REQUIRE((flag & my_enum_2::a) == my_enum_2::a);
+    REQUIRE((flag & my_enum_2::b) != my_enum_2::b);
+    REQUIRE((flag & my_enum_2::c) != my_enum_2::c);
+
+    flag &= ~my_enum_2::a;
+
+    REQUIRE((flag & my_enum_2::a) != my_enum_2::a);
+    REQUIRE((flag & my_enum_2::b) != my_enum_2::b);
+    REQUIRE((flag & my_enum_2::c) != my_enum_2::c);
+}
